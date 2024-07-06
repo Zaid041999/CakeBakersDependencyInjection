af034ac6392f00325736fbc098fddd5fbe1eddc2
Zaid <zaid041999@gmail.com>
2024-07-06 22:58:02 +0530

    first commit

---
+++ src/main/java/com/assignment1/cakebaker/week1/CakeBakerAssignment/CakeBakerAssignmentApplication.java
@@ -0,0 +1,13 @@
+package com.assignment1.cakebaker.week1.CakeBakerAssignment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CakeBakerAssignmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CakeBakerAssignmentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/assignment1/cakebaker/week1/CakeBakerAssignment/CakeBakerAssignmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.assignment1.cakebaker.week1.CakeBakerAssignment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CakeBakerAssignmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
